44cf7d4ab3cc80b8bc7ee99a72c4e438f33e3d55
minmunui <ehdwls1638@gmail.com>
2026-01-22 22:32:11 +0900

    refactor : move schema code that is not proper to notion.py

--- backend/src/services/notion/notion.py
+++ backend/src/services/notion/notion.py
@@ -1,5 +1,6 @@
 import httpx
 from enum import Enum
+from backend.src.services.notion.schema import PropType
 from src.utils.env import get_env
 
 
@@ -12,6 +13,10 @@ class DatabaseType(Enum):
 
 
 class Notion:
+    """
+    Notion API 요청을 위한 싱글톤 패턴의 클래스입니다.
+    이 파일의 notion_client를 import하여 다른 파일에서 사용하십시오.
+    """
     def __init__(self):
         self.base_url = "https://api.notion.com/v1"
         self.client = httpx.AsyncClient()
@@ -35,9 +40,10 @@ class Notion:
     def change_version(self, version: str):
         self.update_header({"Notion-Version": version})
 
-    async def check_health(self):
+    async def check_health(self) -> bool:
         response = await self.client.get(f"{self.base_url}/users/me")
         response.raise_for_status()  # 오류 발생 시 예외 처리
+        return True
 
     async def get_database(self, id: str):
         """데이터베이스 정보 조회"""
@@ -47,7 +53,7 @@ class Notion:
             return response.json()
         except httpx.HTTPStatusError as e:
             print(f"Failed to get database {id}: {e}")
-            raise
+            raise e
 
     async def _load_database_source(self, db_type: DatabaseType, env_key: str):
         """데이터베이스 로드 및 source ID 저장"""
@@ -105,6 +111,83 @@ class Notion:
     
     async def close(self):
         await self.client.aclose()
+
+
+#
+class NotionDBInvalidPropError(Exception):
+    def __init__(self, message: str, detail: dict = {}):
+        super().__init__(message)
+        self.message = message
+        self.detail = detail
+
+    def __str__(self):
+        return f"NotionDBInvalidPropError({self.message}, {self.detail})"
+
+
+# Notion Database 검증을 위해서 사용
+def validate_db(target: dict, condition: dict) -> bool:
+    """
+    notion db가 조건에 맞게 구성되어 있는지 검증합니다.
+    맞지 않는 형태가 있을 경우, 에러 메시지들을 반환합니다.
+
+    :param target: 현재 db data_source.properties
+    :type target: dict
+    :param condition: data_source 조건 dictionary
+    :type condition: dict
+    :return: 검증 결과, 검증이 맞지 않을 경우, Error를 raise하고, 맞을 경우, True를 반환합니다.
+    :rtype: bool
+    """
+    errors = []
+
+    target = (
+        target.get("properties") if target.get("properties") is not None else target
+    )
+
+    for c_key, c_item in condition.items():
+        c_type = c_item["type"]
+        t_value = target.get(c_key, {})
+        t_type = t_value.get("type")
+        # 존재 검증
+        if t_type is None:
+            errors.append(f"속성 {c_key}가 존재하지 않습니다.")
+            continue
+        # 타입 검증
+        if c_type.value != t_type:
+            errors.append(f"속성 {c_key}의 타입은 {c_type.value}이어야 합니다.")
+            continue
+
+        # relation 검증
+        if c_type == PropType.relation:
+            c_relations = c_item.get("relation")
+            t_relations = t_value.get("relation")
+            # condition의 relation 값과 target의 relation 값이 일치하는지 확인
+            for c_rel_key, c_rel_value in c_relations.items():
+                t_rel_value = t_relations.get(c_rel_key).replace("-", "")
+                if t_rel_value != c_rel_value:
+                    errors.append(
+                        f"속성 {c_key}의 관계가 제대로 연결되지 않았습니다. "
+                        f"ID `{t_rel_value}가 아닌 {c_rel_value}`와 연결되어야 합니다."
+                    )
+        # select 검증
+        if c_type == PropType.select:
+            c_selects = c_item.get("select")
+            # notion api로 부터 select 속성의 options 가져오기
+            t_selects = [i.get("name") for i in t_value.get("select").get("options")]
+            # condition의 select값이 target에 모두 존재하는지 확인
+            for c_sel in c_selects:
+                if c_sel not in t_selects:
+                    errors.append(
+                        f"속성 {c_key}에 선택지 {c_sel}가 존재하지 않습니다. "
+                        f"존재하는 선택지 : {t_selects}"
+                    )
+
+    if errors:
+        raise NotionDBInvalidPropError(
+            "Notion Database가 유효하지 않습니다. Notion의 Database가 제대로 설정되었는지 확인하십시오.",
+            {"errors": errors},
+        )
+    else:
+        return True
 
 
 # 싱글톤 패턴
